08b2f144b469dcf76650f121461ec0564c4b2a43
eslamo98 <63984662+eslamo98@users.noreply.github.com>
2023-11-13 11:27:27 +0200

    Update Package.jsx

--- src/components/Packages/Package.jsx
+++ src/components/Packages/Package.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart, faEye, faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
 
 
-const Package = ({image}) => {
+const Package = ({image, meal}) => {
   return (
     <div className='package'>
         <a href='#' className="see-package">
@@ -13,7 +14,7 @@ const Package = ({image}) => {
           <FontAwesomeIcon icon={faHeart} />
         </a>
         <img src={image} alt="image" />
-        <span className="package-name">Tasty Food</span>
+        <span className="package-name">{meal.strMeal}</span>
         <span className="package-details">Lorem Ipsum Dolor Sit, Amet Consectetur Adipisicing Elit. Numquam, Eos.</span>
         <div className="package-rating">
           <FontAwesomeIcon icon={faStar} />
@@ -23,7 +24,10 @@ const Package = ({image}) => {
           <FontAwesomeIcon icon={faStarHalfAlt} />
         </div>
         <span className="package-price">$250 - $450</span>
-        <a href="#" className='custom-btn package-btn'>Learn More</a>
+        <Link 
+             to={`${meal.idMeal}`} className="custom-btn package-btn">
+            More details
+        </Link>
 
     </div>
   )
