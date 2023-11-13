e9c967259f4a7908b546a915489974e2e0c9ab67
eslamo98 <63984662+eslamo98@users.noreply.github.com>
2023-11-13 11:52:34 +0200

    Update Dish.jsx

--- src/components/Dishes/Dish.jsx
+++ src/components/Dishes/Dish.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart, faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
 
-const Dish = ({image}) => {
+const Dish = ({image, meal}) => {
   return (
     <div className='dish'>
         <a href='#' className="love-dish">
@@ -19,11 +20,14 @@ const Dish = ({image}) => {
           <FontAwesomeIcon icon={faStar} />
           <FontAwesomeIcon icon={faStarHalfAlt} />
         </div>
-        <span className="dish-name">Tasty Food</span>
+        <span className="dish-name">{meal.strMeal}</span>
         <span className="dish-details">Lorem Ipsum Dolor Sit, Amet Consectetur Adipisicing Elit. Numquam, Eos.</span>
         
         <span className="dish-price">$250 - $450</span>
-        <a href="#" className='custom-btn dish-btn'>Learn More</a>
+        <Link 
+             to={`${meal.idMeal}`} className="custom-btn dish-btn">
+            More details
+        </Link>
 
     </div>
   )
