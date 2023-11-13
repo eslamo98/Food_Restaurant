1ca2a4aa53841eec9881affcb7f9073f189b15fc
eslamo98 <63984662+eslamo98@users.noreply.github.com>
2023-11-13 11:26:42 +0200

    Update Packages.jsx

--- src/components/Packages/Packages.jsx
+++ src/components/Packages/Packages.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 import "./packages.scss";
 import img1 from "./images/dish-1.png";
 import img2 from "./images/dish-2.png";
@@ -7,8 +8,23 @@ import img4 from "./images/dish-4.png";
 import img5 from "./images/dish-5.png";
 import img6 from "./images/dish-6.png";
 import Package from "./Package";
+import Loader from '../Loader/Loader';
 
 const Packages = ({windowSize}) => {
+  const [popMeals, setPopMeals] = useState([]);
+
+  useEffect(()=>{
+    axios.get('https://www.themealdb.com/api/json/v1/1/search.php?f=b')
+  .then(response => {
+    
+    setPopMeals(response.data.meals.slice(4, 10))
+})
+  
+  .catch(error => console.log('error', error));
+},[]);
+
+console.log(popMeals);
+  
   return (
     <section id='shop' className='packages' style={{padding: windowSize > 1200 ? "5rem 9%" : "5rem 1rem"}}>
       <span className="our-dishes">Our Dishes</span>
@@ -16,12 +32,11 @@ const Packages = ({windowSize}) => {
         POPULAR DISHES
         </h2>
         <div className="packages-container">
-            <Package image={img1}/>
-            <Package image={img2}/>
-            <Package image={img3}/>
-            <Package image={img4}/>
-            <Package image={img6}/>
-            <Package image={img6}/>
+       
+          {popMeals.length == 0 ? <div style={{margin: "atuo"}}><Loader /></div> : popMeals.map(meal => (
+            <Package key={meal.idMeal} image={meal.strMealThumb} meal={meal}/>
+          ))}
+            
         </div>
     </section>
   )
